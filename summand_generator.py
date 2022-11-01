733283e2998c83424d79f68f4cc2fe17ac383e2e
abidkyo <kyotasuna95@gmail.com>
2022-11-01 18:23:58 +0100

    ENH digit exclusion

--- summand_generator.py
+++ summand_generator.py
@@ -2,18 +2,24 @@
 """Summand Generator Function."""
 
 
-def summand_generator(sum, len=2):
+def summand_generator(sum, len=2, excl=None):
+    excl = [] if not excl else excl
+
     # Base case: length < 2.
     if len < 2:
-        # Check for single-digit condition here.
-        if 1 <= sum <= 9:
+        # Check for single-digit condition and excluded digit here.
+        if 1 <= sum <= 9 and sum not in excl:
             return [(sum,)]
         else:
             return []
 
     res = []
     for i in range(1, sum // len + 1):
-        tmp = summand_generator(sum - i, len - 1)
+        # skip number in exclude
+        if i in excl:
+            continue
+
+        tmp = summand_generator(sum - i, len - 1, excl)
 
         for j in tmp:
             # make the tuple and append to res
