de37f6be36932f7e3242a11f59d6b119d4cc8d58
abidkyo <kyotasuna95@gmail.com>
2022-11-01 17:59:49 +0100

    ENH implement algorithm for length more than 2

--- summand_generator.py
+++ summand_generator.py
@@ -3,14 +3,26 @@
 
 
 def summand_generator(sum, len=2):
+    # Base case: length < 2.
+    if len < 2:
+        # Check for single-digit condition here.
+        if 1 <= sum <= 9:
+            return [(sum,)]
+        else:
+            return []
+
     res = []
     for i in range(1, sum // len + 1):
-        j = sum - i
+        tmp = summand_generator(sum - i, len - 1)
 
-        # make the tuple and append to res
-        # if number between 1 and 9 and the tuple not yet in res
-        summands = (i, j)
-        if 1 <= j <= 9 and summands not in res:
-            res.append(summands)
+        for j in tmp:
+            # make the tuple and append to res
+            # if the tuple not yet in res
+            summands = (i,) + j
+            if summands not in res:
+                res.append(summands)
+
+    # Filter is needed for len > 3 because duplicates are unavoidable
+    res = list(set(map(tuple, map(sorted, res))))
 
     return res
