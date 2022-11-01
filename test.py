f89cf6ab0b98164e048d25aaf822ebde9dabf2a3
abidkyo <kyotasuna95@gmail.com>
2022-11-01 18:45:44 +0100

    ENH add test for unique digits summands

--- test.py
+++ test.py
@@ -30,6 +30,30 @@ res_2numbers_repeating = [
     [],
 ]
 
+res_2numbers_unique = [
+    [],
+    [],
+    [],
+    [(1, 2)],
+    [(1, 3)],
+    [(1, 4), (2, 3)],
+    [(1, 5), (2, 4)],
+    [(1, 6), (2, 5), (3, 4)],
+    [(1, 7), (2, 6), (3, 5)],
+    [(1, 8), (2, 7), (3, 6), (4, 5)],
+    [(1, 9), (2, 8), (3, 7), (4, 6)],
+    [(2, 9), (3, 8), (4, 7), (5, 6)],
+    [(3, 9), (4, 8), (5, 7)],
+    [(4, 9), (5, 8), (6, 7)],
+    [(5, 9), (6, 8)],
+    [(6, 9), (7, 8)],
+    [(7, 9)],
+    [(8, 9)],
+    [],
+    [],
+    [],
+]
+
 res_3numbers_repeating = [
     [],
     [],
@@ -166,6 +190,11 @@ class TestSummand2Number(unittest.TestCase):
         self.assertCountEqual(summand_generator(8, 2, [3]), [(1, 7), (2, 6), (4, 4)])
         self.assertCountEqual(summand_generator(9, 2, [2]), [(1, 8), (3, 6), (4, 5)])
         self.assertCountEqual(summand_generator(10, 2, [1]), [(2, 8), (3, 7), (4, 6), (5, 5)])
+
+    def test_unique(self):
+        for i in range(0, len(res_2numbers_unique)):
+            res = summand_generator(i, unique=True)
+            self.assertCountEqual(res, res_2numbers_unique[i])
 
 
 class TestSummand3Number(unittest.TestCase):
